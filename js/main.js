6af7c32e4394aa46506850c538fd9ba8f053becb
ellenlai <miss.ellenlai@gmail.com>
2016-09-17 11:27:06 -0400

    Adding unique IDs to words

--- js/main.js
+++ js/main.js
@@ -11,16 +11,23 @@ $.getJSON(url, function(data)
 
     var words = $(this).text().split(' ');
 
-    $(this).empty().html(function() {
 
+    $(this).empty().html(function() {
       for (i = 0; i < words.length; i++) {
         if (i == 0) {
-          $(this).append('<span>' + words[i] + '</span>');
+          $(this).append('<span class="verse-text">' + words[i] + '</span>' + '<br>');
         } else {
-          $(this).append(' <span>' + words[i] + '</span>');
+          $(this).append('<span class="verse-text">' + words[i] + '</span>' + '<br>');
         }
       }
 
     });
+
+    for (i = 0; i < words.length; i++) {
+      document.getElementsByTagName('span')[i].setAttribute('id', "word" + (i + 1));
+    }
   });
 });
+
+
+
